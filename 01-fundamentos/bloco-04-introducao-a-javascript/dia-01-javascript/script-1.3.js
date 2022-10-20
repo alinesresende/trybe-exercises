7cb6549abd3d9c5a6bb30f597a0728ea50848341
Aline <alinesresende14@gmail.com>
2022-10-20 19:22:58 -0300

    Testei e finalizei o exercício de operador logico ||

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript/script-1.3.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript/script-1.3.js
@@ -1,19 +1,28 @@
 // Exercícios Operadores Lógicos
 
-//Exercício 1
-const currentHour = 15;
-let message = '';
+//Exercício Operador Logico &&
+// const currentHour = 15;
+// let message = '';
 
-if (currentHour >= 22) {
-  message ='Não deveríamos comer nada, é hora de dormir'
-} else if (currentHour >= 18 && currentHour < 22) {
-  message = 'Rango da noite, vamos jantar :D'
-} else if (currentHour >= 14 && currentHour < 18) {
-  message = 'Vamos fazer um bolo pro café da tarde'
-} else if (currentHour >11 & currentHour <14) {
-  message = 'Hora do almoço!!!'
+// if (currentHour >= 22) {
+//   message ='Não deveríamos comer nada, é hora de dormir'
+// } else if (currentHour >= 18 && currentHour < 22) {
+//   message = 'Rango da noite, vamos jantar :D'
+// } else if (currentHour >= 14 && currentHour < 18) {
+//   message = 'Vamos fazer um bolo pro café da tarde'
+// } else if (currentHour >11 & currentHour <14) {
+//   message = 'Hora do almoço!!!'
+// } else {
+//   message ='Hmmm, cheiro de café recém-passado'
+// }
+
+// console.log(message)
+
+//Exercício Operador Logico ||
+let weekDay = 'sábado'
+
+if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay == 'sexta-feira') {
+  console.log('Oba, mais um dia de aprendizado na Trybe >:D') 
 } else {
-  message ='Hmmm, cheiro de café recém-passado'
+  console.log('FINALMENTE, descanso merecido UwU')
 }
-
-console.log(message)
