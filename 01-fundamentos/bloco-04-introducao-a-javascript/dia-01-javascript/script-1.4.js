5a75263885987ab08f061f51bff1156c2c65f7ee
Aline <alinesresende14@gmail.com>
2022-10-21 10:00:28 -0300

    exericico sobre angulos do tringulo

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript/script-1.4.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript/script-1.4.js
@@ -49,3 +49,19 @@ if (valorRecebido > 0) {
 } else {
   console.log('zero')
 }
+
+// Utilize if/else para fazer um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. 
+//Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. 
+//Se algum ângulo for inválido, o programa deve retornar uma mensagem de erro.
+//Para os ângulos serem de um triângulo válido, a soma dos três deve ser 180 graus.
+// Um ângulo será considerado inválido se não tiver um valor positivo.
+
+const a = 60;
+const b = 60;
+const c = 60;
+
+if ((a <= 0 || b <=0 || c <= 0) || (a + b + c != 180)){
+  console.log('inválido')
+} else {
+  console.log('válido')
+}
