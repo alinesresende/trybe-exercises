42fd074c2aa445f357bc7beb1f3a3f68173fa315
Aline <alinesresende14@gmail.com>
2022-10-21 10:37:18 -0300

    Exercicio converso de nota em porcentagem

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript/script-1.4.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript/script-1.4.js
@@ -65,3 +65,30 @@ if ((a <= 0 || b <=0 || c <= 0) || (a + b + c != 180)){
 } else {
   console.log('válido')
 }
+
+// Utilize if/else para fazer um programa que converta uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+// Porcentagem >= 90 -> A
+// Porcentagem >= 80 -> B
+// Porcentagem >= 70 -> C
+// Porcentagem >= 60 -> D
+// Porcentagem >= 50 -> E
+// Porcentagem < 50 -> F
+// O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.D
+
+const notaDada = 101;
+
+if (notaDada < 0 || notaDada > 100) {
+  console.log('erro')
+} else if (notaDada >= 90) {
+  console.log('A')
+} else if (notaDada >=80) {
+  console.log('B')
+} else if (notaDada >=70) {
+  console.log('C')
+} else if (notaDada >=60) {
+  console.log ('D')
+} else if (notaDada >=50) {
+  console.log('E')
+} else if (notaDada <50) {
+  console.log('F')
+}
