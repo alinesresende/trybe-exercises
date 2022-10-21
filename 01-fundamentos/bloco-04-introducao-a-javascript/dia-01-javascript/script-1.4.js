ffcc4ecff04c9fde76f525f1370f1e086c6d1686
Aline <alinesresende14@gmail.com>
2022-10-21 08:26:27 -0300

    Exercicio 1.4

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript/script-1.4.js
@@ -0,0 +1,11 @@
+//Exercício 1
+const a = 8;
+const b = 4;
+
+const adicao = a + b;
+const subtracao = a - b;
+const multiplicacao = a * b;
+const divisao = a / b;
+const modulo = a % b;
+
+console.log(adicao, subtracao, multiplicacao, divisao, modulo)
