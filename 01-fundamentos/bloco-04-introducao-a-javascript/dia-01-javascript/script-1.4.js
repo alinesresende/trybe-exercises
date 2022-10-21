ff63fa7a3eec91d15f6290b4d574363a99aa78ce
Aline <alinesresende14@gmail.com>
2022-10-21 09:35:01 -0300

    Testei os exercicios de if/else

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript/script-1.4.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-01-javascript/script-1.4.js
@@ -36,3 +36,16 @@ if (fistNumber > twoNumber && fistNumber > threeNumber) {
 } else {
   console.log ('A terceira constante é maior que a primeira e segunda')
 }
+
+// Utilize if/else para fazer um programa que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo.
+// e caso não seja nem positivo e nem negativo retorne “zero”.
+
+let valorRecebido = -4;
+
+if (valorRecebido > 0) {
+  console.log('positivo') 
+} else if (valorRecebido < 0) {
+  console.log('negativo')
+} else {
+  console.log('zero')
+}
