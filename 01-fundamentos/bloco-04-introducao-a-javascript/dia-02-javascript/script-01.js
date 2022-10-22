7d38a47d54567671fa8b9ef68fcf2c0271a28f3f
Aline <alinesresende14@gmail.com>
2022-10-22 12:56:43 -0300

    Finalizado exercícios do conteúdo de ARRAYS

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-02-javascript/script-01.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-02-javascript/script-01.js
@@ -1,17 +1,17 @@
-// // 1 - Obtenha o valor “Serviços” do array menu:
+// 1 - Obtenha o valor “Serviços” do array menu:
 
-// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-// let menuServices = menu[1];
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
 
-// console.log(menuServices);
+console.log(menuServices);
 
 
-// // 2 - Procure o índice do valor “Portfólio” do array menu:
+// 2 - Procure o índice do valor “Portfólio” do array menu:
 
-// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-// let indexOfPortfolio = menu[2];
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu[2];
 
-// console.log(indexOfPortfolio);
+console.log(indexOfPortfolio);
 
 // 3 - Adicione o valor “Contato” no final do array menu:
 
