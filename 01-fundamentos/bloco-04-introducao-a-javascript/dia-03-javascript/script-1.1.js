6b0cf51bc071b6241d3007ff536a4ad21f123dbe
Aline <alinesresende14@gmail.com>
2022-10-25 11:36:02 -0300

    Exercicios do dia 03, finalizados

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-03-javascript/script-1.1.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-03-javascript/script-1.1.js
@@ -48,3 +48,24 @@ for (index = 0; index < array.length; index +=1) {
   }
 }
 console.log(palavraMenor)
+console.log(palavraMaior)
+
+
+// Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. 
+// Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 2 e 50.
+
+let numeroPrimo = 0;
+
+for (let index = 2; index <= 50; index += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (index % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    numeroPrimo= currentNumber;
+  }
+}
+
+console.log(numeroPrimo);
