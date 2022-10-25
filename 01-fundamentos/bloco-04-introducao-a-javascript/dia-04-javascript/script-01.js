0ec3f7e63aa5eb941600ce62d92537ec5f30133e
Aline <alinesresende14@gmail.com>
2022-10-25 13:18:47 -0300

    Primeiro exercício do course

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-04-javascript/script-01.js
@@ -0,0 +1,18 @@
+// Crie uma variável player e atribua um objeto contendo as variáveis listadas abaixo:
+
+let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 };
+
+let player = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: {
+    golden: 2,
+    silver: 3,
+  }
+}
+
+console.log(player)
