911e3a7ef63170d9eddaa324d9a4d4f9c6e8f9b6
Aline <alinesresende14@gmail.com>
2022-10-25 11:21:32 -0300

    Exercicio palavra invertida, concluido

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-03-javascript/script-1.1.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-03-javascript/script-1.1.js
@@ -13,3 +13,17 @@ for (let index = 1; index < number; index +=1) {
 }
 
 console.log(resultado)
+
+
+// Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. 
+// Por exemplo, a palavra “banana” seria invertida para “ananab”. 
+// Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+
+let word = 'tryber';
+let palavraInvertida = '';
+
+for (index = 0; index < word.length; index +=1) {
+  palavraInvertida = palavraInvertida + word[word.length - 1 - index];
+}
+
+console.log (palavraInvertida)
