b91fce7e3721dc33d4347a83cdfd206a32ddca52
Aline <alinesresende14@gmail.com>
2022-10-25 21:32:29 -0300

    Finalizados exercícios de objetos for/in for/of

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-04-javascript/script-1.2.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-04-javascript/script-1.2.js
@@ -61,3 +61,21 @@ for (let index in info) {
       },
     ],
   };
+
+  /* Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, 
+  e faça um console.log no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”. */
+
+  console.log('O livro favorito de ' + leitor.nome, leitor.sobrenome, 'se chama ' + leitor.livrosFavoritos[0].titulo) 
+
+  /* Adicione um novo livro favorito na chave livrosFavoritos, que é um array. 
+  Atribua a essa chave um objeto contendo as seguintes informações: */
+
+  leitor.livrosFavoritos.push(
+    {
+      titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+      autor: 'JK Rowling',
+      editor: 'Rocco',
+    },
+  );
+
+  console.log(leitor['nome'] + ' tem ' + leitor['livrosFavoritos'].length + ' livros favoritos.');
