d127d08faa82273f4bba2fa947d2fcd5a935c0da
Aline <alinesresende14@gmail.com>
2022-10-27 14:11:49 -0300

    Exercícios de funções

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-04-javascript/script-1.3.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-04-javascript/script-1.3.js
@@ -65,3 +65,14 @@ console.log(indiceDoMenor([2, 4, 6, 7, 10, 0, -3]))
 /*Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
 Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];.
 Valor esperado no retorno da função: Fernanda. */
+
+function maiorPalavra (nomes) {
+  let caracters = nomes[0];
+  for (let index in nomes){
+    if(caracters.length < nomes[index].length) {
+      caracters = nomes[index];
+    }
+  }
+  return caracters;
+}
+console.log(maiorPalavra(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']))
