435aa21ae89dd731fa28b76e85c9206ca7303123
Aline <alinesresende14@gmail.com>
2022-10-27 12:03:17 -0300

    Exercícios de functions, em andamento

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-04-javascript/script-1.3.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-04-javascript/script-1.3.js
@@ -1,37 +1,67 @@
-// Exercícios Funções 
+// // Exercícios Funções 
 
-/* Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
+// /* Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
 
-Exemplo de palíndromo: arara
+// Exemplo de palíndromo: arara
 
-verificaPalindrome('arara')
+// verificaPalindrome('arara')
 
-Retorno esperado: true
-verificaPalindrome('desenvolvimento')
+// Retorno esperado: true
+// verificaPalindrome('desenvolvimento')
 
-Retorno esperado: false */
+// Retorno esperado: false */
 
-function verificaPalindrome(word){
-  for(index in word) {
-    if(word[index] != word[(word.length - 1) - index]) {
-      return false;
+// function verificaPalindrome(word){
+//   for(index in word) {
+//     if(word[index] != word[(word.length - 1) - index]) {
+//       return false;
+//   }
+// }
+// return true;
+// }
+
+// /// Outro metodo
+
+// function verificaPalindrome(string) {
+//   let reverse = string.split('').reverse().join('');
+  
+//   return reverse
+// }
+
+// console.log(verificaPalindrome('arara')); //true
+// console.log(verificaPalindrome('desenvolvimento')); //false
+
+
+/* Crie uma função que receba um array de inteiros e retorne o índice do maior valor */
+
+function acharMaiorIndice (numeros) {
+  let indiceMaior = 0;
+  for(let indice in numeros) {
+    if (numeros[indiceMaior] < numeros[indice]) {
+      indiceMaior = indice;
   }
 }
-return true;
+return indiceMaior;
+}
+console.log(acharMaiorIndice([2, 3, 6, 7, 10, 1]))
+
+
+/* Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+Array de teste: [2, 4, 6, 7, 10, 0, -3];.
+Valor esperado no retorno da função: 6. */
+
+function indiceDoMenor(numbers) {
+  let indiceMenor = 0;
+  for (let indice in numbers) {
+    if (numbers[indiceMenor] > numbers[indice]) {
+      indiceMenor = indice;
+    }
+  }
+  return indiceMenor;
 }
 
-/// Outro metodo
+console.log(indiceDoMenor([2, 4, 6, 7, 10, 0, -3]))
 
-function verificaPalindrome(string) {
-  let reverse = string.split('').reverse().join('');
-  
-  return reverse
-}
-
-console.log(verificaPalindrome('arara')); //true
-console.log(verificaPalindrome('desenvolvimento')); //false
-
-
-const fruits = ['Apple', 'Banana'];
-const fruitsString = fruits.join(', ');
-console.log(fruitsString);
+/*Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];.
+Valor esperado no retorno da função: Fernanda. */
