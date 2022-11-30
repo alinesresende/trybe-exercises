a9928b1aef054ffaad3f61e7ea4961e50045f703
Aline <alinesresende14@gmail.com>
2022-11-30 20:04:06 -0300

    Correção de formatação.

--- 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-higher-order-functions/hofs.js
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-higher-order-functions/hofs.js
@@ -1,15 +1,15 @@
 // Exercícios parte I
+
 const pessoaContratada = (nome) => {
-return {Nome: nome, email: `${nome}@trybe.com`};
+  return { Nome: nome, email: `${nome}@trybe.com` };
 };
-console.log(pessoaContratada('alineresende'))
 
 const newEmployees = (pessoaContratada) => {
   const employees = {
-    id1: pessoaContratada('PedroGuerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: pessoaContratada('LuizaDrumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: pessoaContratada('CarlaPaiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-  }
+    id1: pessoaContratada("PedroGuerra"), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: pessoaContratada("LuizaDrumond"), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: pessoaContratada("CarlaPaiva"), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  };
   return employees;
 };
 console.log(newEmployees(pessoaContratada));
