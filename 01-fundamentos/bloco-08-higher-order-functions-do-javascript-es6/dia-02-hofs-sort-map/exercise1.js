4c03bb4c578642272412367909cbb43a6d61dfba
Aline <alinesresende14@gmail.com>
2022-12-02 21:09:09 -0300

    Exercício de criar um array com strings e objeto, finalizado.

---
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-hofs-sort-map/exercise1.js
@@ -0,0 +1,93 @@
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA:
+
+const expectedResult = [
+  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  'Fundação - Ficção Científica - Isaac Asimov',
+  'Duna - Ficção Científica - Frank Herbert',
+  'A Coisa - Terror - Stephen King',
+  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+];
+
+// Exemplo retornando string:
+const formatedBookNames = () => {
+  const newArray = books.map((livro) => {
+    return `${livro.name} - ${livro.genre} - ${livro.author.name}`
+  })
+  return newArray;
+};
+console.log(formatedBookNames())
+
+// Exemplo retornando objeto:
+const formatedBookNames2 = () => {
+  const newArray = books.map((livro) => {
+    return {name: livro.name,
+      genero: livro.genre,
+      Autor: livro.author.name,}
+    })
+    return newArray;
+};
+console.log(formatedBookNames2())
