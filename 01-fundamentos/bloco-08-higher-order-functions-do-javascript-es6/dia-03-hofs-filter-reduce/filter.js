20bb2c9d4cc212eeed20613ab3852c7acff710d8
Aline <alinesresende14@gmail.com>
2022-12-03 13:33:53 -0300

    Conteúdo de filter.

--- 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-hofs-filter-reduce/filter.js
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-hofs-filter-reduce/filter.js
@@ -18,3 +18,15 @@ dados.filter((item) => {
 const dados1 = ['Luca', 91234567, 'Ana', 92345678, 'Marlete', 93456789];
 
 dados1.filter((item) => typeof item === 'string');
+
+
+// filter array com numero maior de 20:
+const listaNumeros = [10, 20, 30, 40];
+
+// Armazena o resultado em uma nova variável maiorVinte
+let maiorVinte = listaNumeros.filter((item) => {
+  // retorne apenas os elementos que são maiores do que vinte
+  return item > 20;
+});
+
+return maiorVinte;
