7bfeea8dd38a7f9a1a1c1339041e42e289421ac4
Aline <alinesresende14@gmail.com>
2022-12-05 19:27:26 -0300

    Exercício 05, finalizado.

--- 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-hofs-filter-reduce/desafio-pratico.js
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-hofs-filter-reduce/desafio-pratico.js
@@ -143,3 +143,32 @@ const countA = () => {
  return quantLetraA;
 };
 countA();
+
+// 5 - Crie um array de objetos e calcule a média de notas. 
+// Utilize as constantes students e grades para criar um array de objetos e calcule a média da nota das pessoas estudantes:
+// O index 0 do array `students` equivale ao index 0 do array `grades`
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+const expectedResult5 = [
+ { name: 'Pedro Henrique', average: 7.8 },
+ { name: 'Miguel', average: 9.2 },
+ { name: 'Maria Clara', average: 8.8 },
+];
+
+const studentAverage = () => {
+  const newArray = students.map((name, index) => {
+    const notas = grades[index].reduce((acc, curr) => {
+     return acc + curr
+    }, 0)
+    let media = notas / grades[index].length;
+    let newArrayObj = {
+      name: name,
+      average: media,
+    }
+    return newArrayObj;
+  });
+  return newArray;
+};
+studentAverage();
