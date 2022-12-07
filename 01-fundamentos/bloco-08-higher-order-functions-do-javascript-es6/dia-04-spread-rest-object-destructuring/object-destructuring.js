41dd3813af739647800e16387b5d224c695e7d2d
Aline <alinesresende14@gmail.com>
2022-12-07 11:45:44 -0300

    Exercício de fixação.

--- 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-spread-rest-object-destructuring/object-destructuring.js
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-spread-rest-object-destructuring/object-destructuring.js
@@ -1,1 +1,85 @@
 // Conteúdo de Object Destructuring
+
+// A sintaxe da desestruturação de objetos pede para passarmos o nome das propriedades que queremos acessar do lado esquerdo,
+// entre chaves, e o objeto do lado direito
+
+const product = {
+  name: 'Smart TV Crystal UHD',
+  price: '1899.05',
+  seller: 'Casas de Minas',
+};
+
+const { name } = product;
+console.log(name); // Smart TV Crystal UHD
+
+// Se quisermos pegar, além do nome, o vendedor do produto, 
+// podemos incluir a propriedade seller dentro das chaves para acessar o seu valor correspondente:
+
+const { name, seller } = product;
+console.log(name); // Smart TV Crystal UHD
+console.log(seller); // Casas de Minas
+
+// Exemplo de um objeto dentro de outro objeto:
+// definindo o objeto
+const character = {
+  name: 'Luke SkyWalker',
+  age: '53',
+  description: {
+    specieName: 'Human',
+    jedi: true,
+  },
+  homeWorld: {
+    name: 'Tatooine',
+    population: '200000',
+  },
+};
+
+// desestruturando o objeto:
+const { name, age, homeWorld: { name: planetName }, description: { jedi } } = character;
+
+// imprimindo os valores:
+console.log(`Esse é o ${name}, ele tem ${age} anos, mora no planeta ${planetName} e, por incrível que possa parecer, ele ${jedi ? 'é um Jedi' : 'não é um Jedi'}.`);
+
+//Segue a sintaxe: homeWorld: { name: planetName }. Agora planetName é uma variável que recebe o valor da propriedade name do objeto homeWorld.
+
+// Outro exemplo utilizando spread:
+
+const daysOfWeek = {
+  workDays: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+  weekend: ['Saturday', 'Sunday'],
+};
+const { workDays, weekend } = daysOfWeek;
+const weekdays = [...workDays, ...weekend];
+console.log(weekdays)
+
+//IMPORTANTE:
+// Um truque legal dessa feature é que você pode reatribuir o nome da propriedade que deseja acessar ao declará-la como uma variável:
+//sintaxe: { propriedade: nomeVariável } = objeto
+const student = {
+  a: 'Maria',
+  b: 'Turma B',
+  c: 'Matematica',
+};
+
+const { a: name, b: classAssigned, c: subject } = student; // reatribuindo o nome da propriedade
+
+console.log(name); // Maria
+console.log(classAssigned); // Turma B
+console.log(subject); // Matemática
+
+//  outra situação em que podemos usar a desestruturação de objetos é quando queremos passar os valores de um objeto como parâmetros para uma função, como no exemplo abaixo:
+const product = {
+  name: 'Smart TV Crystal UHD',
+  price: '1899.05',
+  seller: 'Casas de Minas',
+};
+
+const printProductDetails = ({ name, price, seller }) => {
+  console.log(`Promoção! ${name} por apenas ${price} é só aqui: ${seller}`);
+};
+
+printProductDetails(product); // Promoção! Smart TV Crystal UHD por apenas 1899.05 é só aqui: Casas de Minas
+// Passa a chava do obj como paramentro e na hora de executar a função chama o nome do objeto.
+
+// Exercício de fixação
+
