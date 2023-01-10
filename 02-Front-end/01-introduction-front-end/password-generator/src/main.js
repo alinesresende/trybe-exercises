2a68988c96a6c581b7ced13ab145847bc7e6c608
Aline <alinesresende14@gmail.com>
2023-01-10 11:44:23 -0300

    Front-end content exercise.

---
+++ 02-Front-end/01-introduction-front-end/password-generator/src/main.js
@@ -0,0 +1,18 @@
+import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
+
+// With vite it is possible to import CSS directly into JavaScript
+import './style.css'
+
+const passwordBtnEl = document.querySelector('button');
+const displayPasswordEl = document.querySelector('h2');
+
+passwordBtnEl.addEventListener('click', () => {
+  const randomPassword = nanoid();
+  displayPasswordEl.innerHTML = randomPassword;
+});
+
+displayPasswordEl.addEventListener('click', (event) => {
+  copy(event.target.innerHTML);
+  alert('Copied password!');
+});
