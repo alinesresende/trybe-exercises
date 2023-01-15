b9a338a1f799909641e68f6e5014c168a008ddfd
Aline <alinesresende14@gmail.com>
2023-01-15 16:32:30 -0300

    Exercise done with .then. Redid the exercise, using async/await.

--- 02-Front-end/01-introduction-front-end/super-hero/src/main.js
+++ 02-Front-end/01-introduction-front-end/super-hero/src/main.js
@@ -6,17 +6,18 @@ const image = document.createElement("img");
 
 button.addEventListener('click', cardsHero)
 
-function cardsHero(){
-  const randomNumber = parseInt(Math.random() * 500)
-  fetch(`https://www.superheroapi.com/api.php/5771591916293646/${randomNumber}`)
-    .then(response => response.json())
-    .then((data) => {
-      image.src = data.image.url
-      imgSuperHero.appendChild(image)
-      textSuperHero.innerHTML = data.name
-    })
-    .catch((error) => {
-     textSuperHero.innerHTML = `Error when making request, ${error.message}`
-    })
+async function cardsHero() {
+  try {
+    const randomNumber = parseInt(Math.random() * 500);
+    const response = await fetch(
+      `https://www.superheroapi.com/api.php/5771591916293646/${randomNumber}`
+    );
+    const dados = await response.json();
+    image.src = dados.image.url;
+    imgSuperHero.appendChild(image);
+    textSuperHero.innerHTML = dados.name;
+  } catch (error) {
+    textSuperHero.innerHTML = `Error when making request, ${error.message}`;
+  }
 }
-window.onload = cardsHero()
+window.onload = cardsHero();
