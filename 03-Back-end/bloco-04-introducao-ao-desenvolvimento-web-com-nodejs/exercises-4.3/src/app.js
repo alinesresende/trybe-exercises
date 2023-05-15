3fd7ea7133d94d563e8ba0e91696851a2886ffbc
ALine Resende <alinesresende14@gmail.com>
2023-05-15 15:17:19 -0300

    Starting exercises - 4.3 Test with mocha and chai.

---
+++ 03-Back-end/bloco-04-introducao-ao-desenvolvimento-web-com-nodejs/exercises-4.3/src/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+
+const fs = require('fs').promises;
+const path = require('path')
+
+const app = express();
+
+const readCacauTrybeFile = async () => {
+  const cacauTrybe = './cacauTrybeFile.json';
+  try {
+    const contentFile = await fs.readFile(path.resolve(__dirname, cacauTrybe), 'utf-8');
+    return JSON.parse(contentFile);
+  } catch (error) {
+    return null;
+  }
+};
+
+const getAllChocolates = async () => {
+  const cacauTrybe = await readCacauTrybeFile();
+  return cacauTrybe.chocolates;
+};
+
+app.get('/chocolates/total', async (req, res) => {
+  const cacauTrybe = await readCacauTrybeFile();
+  console.log(cacauTrybe)
+  const chocolates = cacauTrybe.chocolates;
+  const totalChocolates = chocolates.length;
+  res.status(200).json({
+    totalChocolates
+  })
+})
+
+module.exports = {
+    getAllChocolates,
+};
+
+
+module.exports = app;
