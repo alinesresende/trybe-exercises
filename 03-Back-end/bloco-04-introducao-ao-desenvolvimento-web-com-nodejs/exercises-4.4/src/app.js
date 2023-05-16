bd32e9009e85c80fb28c81b91cf1a0f2caba9ae5
ALine Resende <alinesresende14@gmail.com>
2023-05-16 17:54:44 -0300

    Udpate files.

---
+++ 03-Back-end/bloco-04-introducao-ao-desenvolvimento-web-com-nodejs/exercises-4.4/src/app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+
+const app = express();
+
+let nextId = 3;
+const teams = [
+  { id: 1, nome: 'São Paulo Futebol Clube', sigla: 'SPF' },
+  { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
+];
+
+app.use(express.json());
+
+app.get('/teams', (req, res) => res.json(teams));
+
+// Crie um middleware existingId para garantir que o id passado como parâmetro na rota GET /teams/:id
+// existe no objeto teams. Refatore essa rota para usar o middleware.
+
+// Reaproveite esse middleware e refatore as rotas PUT /teams/:id e DELETE /teams/:id para usarem ele também.
+
+
+const existingId = (req, res, next) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+  if (team) {
+    next();
+} else {
+  res.sendStatus(404);
+}
+}
+app.get('/teams/:id', existingId, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+    res.status(200).json(team);
+});
+
+//Criação do middleware de validação
+
+const validateTeam = (req, res, next) => {
+  const requiredProperties = ['nome', 'sigla'];
+  if (requiredProperties.every((property) => property in req.body)) {
+    next(); // Chama o próximo middleware
+  } else {
+    res.sendStatus(400); // Ou já responde avisando que deu errado
+  }
+};
+
+app.post('/teams', validateTeam, (req, res) => {
+  const team = { id: nextId, ...req.body };
+  teams.push(team);
+  nextId += 1;
+  res.status(201).json(team);
+ 
+});
+
+
+app.put('/teams/:id', validateTeam, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+  const index = teams.indexOf(team);
+  const updated = { id, ...req.body };
+  teams.splice(index, 1, updated);
+  res.status(201).json(updated);
+});
+
+app.delete('/teams/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+  const index = teams.indexOf(team);
+  teams.splice(index, 1);
+
+});
+
+module.exports = app;
---
+++ 03-Back-end/bloco-04-introducao-ao-desenvolvimento-web-com-nodejs/exercises-4.5/trybecash-api/src/server.js
@@ -0,0 +1,9 @@
+// src/server.jss
+
+const app = require('./app');
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
+});
